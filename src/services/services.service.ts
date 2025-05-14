918809dc54765727da4e3e16aa8833549171ef09
Lucas Aristizabal <79490483+lukkaku12@users.noreply.github.com>
2025-05-14 17:56:09 -0500

    fix: brings schedules for services

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -37,14 +37,14 @@ export class ServicesService {
   async findBySellerId(sellerId: number): Promise<Service[]> {
   return this.serviceRepository.find({
     where: { seller: { user_id: sellerId } },
-    relations: ['seller', 'serviceSchedule'],
+    relations: ['seller', 'service_schedule'],
   });
 }
 
   async findOne(id: number): Promise<Service> {
     const service = await this.serviceRepository.findOne({
       where: { service_id: id },
-      relations: ['serviceSchedule'],
+      relations: ['service_schedule'],
     });
 
     if (!service) {
