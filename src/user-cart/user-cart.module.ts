e743d782d8c162e3642db4f33bd7db9f6083bfaa
Lucas Aristizabal <79490483+lukkaku12@users.noreply.github.com>
2025-05-16 15:59:57 -0500

    fix: updated imports

--- src/user-cart/user-cart.module.ts
+++ src/user-cart/user-cart.module.ts
@@ -6,9 +6,10 @@ import { CartItem } from 'src/cart-item-id/entities/cart-item-id.entity';
 import { UserCart } from './entities/user-cart.entity'; // Asegúrate de que este archivo existe
 import { UserCartController } from './user-cart.controller';
 import { UserCartService } from './user-cart.service';
+import { CartItemIdModule } from 'src/cart-item-id/cart-item-id.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserCart, CartItem])], // Importa la entidad en el módulo
+  imports: [CartItemIdModule, TypeOrmModule.forFeature([UserCart, CartItem])], // Importa la entidad en el módulo
   controllers: [UserCartController],
   providers: [UserCartService, CartItemIdService],
   exports: [UserCartService], // Exporta el servicio si lo necesitas en otros módulos
